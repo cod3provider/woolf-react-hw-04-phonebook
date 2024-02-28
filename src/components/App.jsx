bc53a4ae044518f5bc97b1776af56c2b0f9a2072
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-28 13:09:48 +0100

    small fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {nanoid} from 'nanoid';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 
 import ContactForm from './ContactForm/ContactForm';
 import ContactsList from './ContactsList/ContactsList';
@@ -7,56 +7,59 @@ import Filter from './Filter/Filter';
 import Container from './shared/Container/Container';
 
 const App = () => {
-	const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contacts')) || []);
-	const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contacts')) || []);
+  const [filter, setFilter] = useState('');
 
-	useEffect(() => {
-		localStorage.setItem('contacts', JSON.stringify(contacts));
-		setContacts(contacts);
-	}, [contacts]);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-	const createContact = ({name, number}) => {
-		const newContact = {
-			id: nanoid(),
-			name,
-			number,
-		};
+  const createContact = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
 
-		setContacts(prevState => {
-			const isExist = contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
-			if (isExist) {
-				alert(`${name} is already in contacts.`);
-				return false;
-			}
-			return [newContact, ...prevState];
-		})
-	};
+    const isExist = contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
+    if (isExist) {
+      alert(`${name} is already in contacts.`);
+      return false;
+    }
 
-	const handleFilterChange = e => {
-		const {value} = e.target;
-		setFilter(value);
-	};
+    setContacts(prevState => {
+      return [newContact, ...prevState];
+    });
+  };
 
-	const getFilteredContacts = () => {
-		return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-	};
+  const handleFilterChange = e => {
+    const { value } = e.target;
+    setFilter(value);
+  };
 
-	const handleDelete = contactId => {
-		setContacts(prevState => prevState.filter(({id}) => id !== contactId))
-	};
+  const getFilteredContacts = () => {
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  };
 
-	const filteredContacts = getFilteredContacts();
+  const handleDelete = contactId => {
+    setContacts(prevState => prevState.filter(({ id }) => id !== contactId));
+  };
 
-	return (
-		<Container>
-			<h1>Phonebook</h1>
-			<ContactForm onSubmit={createContact}/>
+  const filteredContacts = getFilteredContacts();
 
-			<h2>Contacts</h2>
-			<Filter value={filter} handleChange={handleFilterChange}/>
-			<ContactsList contacts={filteredContacts} onDelete={handleDelete}/>
-		</Container>
-	);
-}
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={createContact} />
+
+      {/* Same as */}
+      <ToastContainer />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} handleChange={handleFilterChange} />
+      <ContactsList contacts={filteredContacts} onDelete={handleDelete} />
+    </Container>
+  );
+};
 
 export default App;
